ad6669cd5b44b739116eceebb2ed2b8d2e4924d0
Daniel <33257@ma-web.nl>
2022-03-21 11:48:54 +0100

    bbdd

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js" defer></script>
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li class="naam">
+                    GreenHome
+                </li>
+                <li class="tijdDatum">
+                    <p id="js--datum"></p>
+                    <p id="js--tijd"></p>
+                </li>
+
+                <li class="logo">
+                    <img class="greenHomeLogo" src="img/greenhome.webp" alt="het logo van green home">
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <canvas class="myChart" id="myChart" width="2000" height="500"></canvas>
+            <canvas id="js--zonnepanelen" width="2000" height="500"></canvas>
+    	
+        </section>
+        <section>
+            <?php
+                    
+                $json = file_get_contents("https://data.buienradar.nl/2.0/feed/json");
+                $data = json_decode($json);
+                $forecast = $data->forecast->fivedayforecast[0];
+
+                $minTemperatuur = $forecast->mintemperatureMin;
+                $maxTemperatuur = $forecast->maxtemperatureMax;
+                $weerBeschrijving = $forecast->weatherdescription;
+                $icon = $forecast->iconurl;
+            ?>
+        <section>
+            <p>
+                Weerbericht van vandaag
+            </p>
+            <figure class="icon">
+                <img src="<?php echo $icon ?>" alt="plaatje van het weer">
+            </figure>
+            <span class="minTemp">
+                <?php
+                    echo "Min" . " " . $minTemperatuur . "&#8451; <br>";
+                ?>
+            </span>
+            <span class="maxTemp">
+                <?php
+                    echo "Max" . " " . $maxTemperatuur . "&#8451; <br>";
+                ?>
+            </span>
+            <p class="weerBeschrijving">
+                <?php
+                    echo $weerBeschrijving;
+                ?>
+        </section>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
+
+    </main>
+    <footer>
+
+    </footer>
+    </div>
+
+
+
+</body>
+
+</html>
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Datum
 var today = new Date();
-var date = today.getDate() +"-"+(today.getMonth()+1)+"-"+ today.getFullYear();
+var date = today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
 
 // tijd
 `use strict`
@@ -10,9 +10,78 @@ document.getElementById("js--tijd").textContent = datetime; //it will print on h
 
 `use strict`;
 function refreshTime() {
-  const timeDisplay = document.getElementById("js--tijd");
-  const dateString = new Date().toLocaleString();
-  const formattedString = dateString.replace(", "," - ");
-  timeDisplay.textContent = formattedString;
+    const timeDisplay = document.getElementById("js--tijd");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
 }
-  setInterval(refreshTime, 1000);
+setInterval(refreshTime, 1000);
+
+// de grafriek Daniel
+
+var ctx = document.getElementById('myChart');
+
+
+
+var stars = [135850, 112122, 148825, 96939, 59763, 8080, 78900];
+var frameworks = ['week 1', 'week2', 'week 3', 'week4 ', 'week5', 'week 6', 'week 7'];
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: frameworks,
+        datasets: [{
+            label: "stroom verbruik per 1 week",
+            data: stars,
+            backgroundColor: [
+                "rgba(102, 255, 102, 0.2)",
+                "rgba(255, 102, 255, 0.2)",
+                "rgba(102, 204, 255, 0.2)",
+                "rgba(255, 0, 0, 0.2)",
+                "rgba(0, 51, 102, 0.2)",
+            ],
+            borderColor: [
+                "rgba(102, 255, 102, 1)",
+                "rgba(255, 102, 255, 1)",
+                "rgba(102, 204, 255, 1)",
+                "rgba(255, 0, 0, 1)",
+                "rgba(0, 51, 102, 1)",
+            ],
+            borderWidth: 2,
+        }]
+    },
+});
+// eind grafriek Daniel
+
+
+//zonnenpanelen grafiek Nick
+var ctx = document.getElementById('js--zonnepanelen');
+var stars = [120000, 80000, 60000, 80000, 9000, 80000, 60000];
+var frameworks = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', ' Vrijdag', 'zaterdag', 'zondag'];
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: frameworks,
+        datasets: [{
+            label: 'Zonnepanelen opbrengst in kWh (dummy cijfers)',
+            data: stars,
+            backgroundColor: [
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+                "rgba(128,0,128)"
+            ],
+            borderColor: [
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+                "rgba(128,0,128)",
+            ],
+            borderWidth: 2,
+            borderRadius: 5
+        }]
+    },
+})
+//eind grafiek Nick
